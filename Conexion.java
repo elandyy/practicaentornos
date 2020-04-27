ab236bace772890189097053c5a66b91f50390d0
Andy <zoarnor9@gmail.com>
2020-04-27 19:12:04 +0200

    Segunda clase (Conexion.java)

---
+++ Conexion.java
@@ -0,0 +1,46 @@
+package agenda;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Conexion{
+	
+	private static final String CONTROLADOR = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/conexionBD";
+    private static final String USUARIO = "root";
+    private static final String CLAVE = "hola";
+    
+    
+    
+    static {
+    	try {
+			Class.forName(CONTROLADOR);
+		} catch (ClassNotFoundException e) {
+			System.out.println("ERROR AL CARGAR EL CONTROLADOR");
+			e.printStackTrace();
+		}
+    }
+    
+    public static Connection conectar() 
+	{
+		Connection conexion = null;
+    	try 
+		{
+			conexion=DriverManager.getConnection(URL,USUARIO,CLAVE);
+			System.out.println("Conectado con éxito");
+		}
+		catch (SQLException e)
+		{
+			System.out.println("ERROR EN LA CONEXIÓN");
+			e.printStackTrace();
+		}
+		return conexion;
+	}
+}
+
+
+
